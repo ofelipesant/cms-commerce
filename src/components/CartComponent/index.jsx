bd054ede6ec3197e954a30956bc74a6da9ca29d7
Felipe dos Santos <ofelipe.dev@gmail.com>
2022-07-07 15:34:55 -0300

    estilizações
    continuação da estilização da página de carrinho

--- src/components/CartComponent/index.jsx
+++ src/components/CartComponent/index.jsx
@@ -1,14 +1,15 @@
 import { useContext } from 'react'
 import {MdShoppingCart} from 'react-icons/md'
 import { commerceContext } from '../../contexts/commerceContext'
+import './cart-component.sass'
 
 export default function CartComponent(){
     const {cart} = useContext(commerceContext)
 
     return(
         <div className="cart-component">
-           <MdShoppingCart/>
-           <span>{cart.length}</span>
+           <MdShoppingCart className='cart-icon'/>
+           <span className='cart-qtd'>{cart.length}</span>
         </div>
     )
 }
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -2,6 +2,7 @@ import { commerceContext } from "../../contexts/commerceContext"
 import { useContext, useEffect } from "react"
 import Cabecalho from "../../components/Cabecalho"
 import { TbTrash } from 'react-icons/tb'
+import './cart.sass'
 
 export default function Cart(){
     const { cart, setCart } = useContext(commerceContext)
@@ -21,6 +22,7 @@ export default function Cart(){
 
         <section className="cart-section">
             <div className="list-products">
+                <div className="cart-card">
                     {cart && cart.map((element, index) => {
                         return(
                             <div key={index} className="cart-products-card">
@@ -28,17 +30,21 @@ export default function Cart(){
                                     <img src={element.image[0].url} alt="produto" />
                                 </div>
                                 <div className="infos">
-                                    <p>{element.name}</p>
-                                    <h3>R${element.price.toFixed(2)}</h3>
-                                    <button>
-                                        <TbTrash/>
+                                    <p className="product-title">{element.name}</p>
+                                    <h3 className="product-price">R${element.price.toFixed(2)}</h3>
+                                    <button className="remove-btn">
+                                        <TbTrash className="remove-icon"/>
                                     </button>
                                 </div>
                             </div>
                         )
                     })}
-                
-                <button>Finalizar pedido</button>
+                </div>
+                <div className="confirm-order">
+                    <label className="total-label">Valor total:</label>
+                    <p className="total-price">R$XX,XX</p>
+                    <button className="confirm-order-btn">FINALIZAR PEDIDO</button>
+                </div>
             </div>
         </section>
         </>
